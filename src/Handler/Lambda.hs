ea8c902373393e635b430630431d661f3e311539
SchrodingerZhu <i@zhuyi.fan>
2019-08-15 23:20:33 +0800

    all most done

--- src/Handler/Lambda.hs
+++ src/Handler/Lambda.hs
@@ -5,12 +5,13 @@
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
 
-module Handler.Lambda where
+module Handler.Lambda (getHeartbeatR, getTraceR, putPutTraceR, postStartTraceR) where
 import           Lambda.Endpoint
 import           Data.Aeson
 import qualified Data.Text          as T 
 import           Import
-import           Lambda.Trace
+import           Lambda.Trace 
+import           Database.Persist.Sql
 
 data HeartbeatReply =
   HeartbeatReply
@@ -19,9 +20,41 @@ data HeartbeatReply =
     }
   deriving (Generic, Show)
 
+data StartTrace = 
+  StartTrace {
+    traceT :: Text,
+    traceId :: Int64,
+    lasting :: Int 
+  } deriving (Generic, Show)
+
+data StartTraceReply = 
+  StartTraceReply {
+    thread :: String,
+    filePath :: String 
+  } deriving (Generic, Show)
+
+data InfoWithId =
+  InfoWithId {
+    info :: TraceInfo,
+    traceNo :: Int64
+  } deriving (Generic, Show)
+
 instance ToJSON HeartbeatReply where
   toEncoding = genericToEncoding defaultOptions
 instance FromJSON HeartbeatReply
+
+
+instance ToJSON StartTrace where
+  toEncoding = genericToEncoding defaultOptions
+instance FromJSON StartTrace
+
+instance ToJSON InfoWithId where
+  toEncoding = genericToEncoding defaultOptions
+instance FromJSON InfoWithId
+
+instance ToJSON StartTraceReply where
+  toEncoding = genericToEncoding defaultOptions
+instance FromJSON StartTraceReply
 
 getHeartbeatR :: Handler Value
 getHeartbeatR = withVerification $ do
@@ -32,10 +65,10 @@ getTraceR :: Handler Value
 getTraceR = withVerification $ do
   m <- runDB $ do
     post <- selectList [] []
-    return $ fmap toInfo ((fmap entityVal post) :: [BPF])
+    return $ fmap (pure InfoWithId <*> toInfo . entityVal <*> (unSqlBackendKey . unBPFKey . entityKey)) post
   n <- runDB $ do
     post <- selectList [] []
-    return $ fmap toInfo ((fmap entityVal post) :: [STap])
+    return $ fmap (pure InfoWithId <*> toInfo . entityVal <*> (unSqlBackendKey . unSTapKey . entityKey)) post
   return $ toJSONList $ m <> n
 
 putPutTraceR :: Handler Text
@@ -48,8 +81,35 @@ putPutTraceR = withVerification $ do
       "STAP" -> do
         db <- runDB $ insert ((fromInfo body) :: STap)
         return $ T.pack $ show db
-      _ -> return "failure"
+      _ -> do 
+        invalidArgs ["traceType"]
   return result
+
+postStartTraceR :: Handler Value
+postStartTraceR = withVerification $ do
+  body <- requireCheckJsonBody :: Handler StartTrace
+  liftIO $ print body
+  case (traceT body) of
+    "BPF" -> do 
+      let key = BPFKey (SqlBackendKey $ Handler.Lambda.traceId body)
+      db <- runDB $ selectList [BPFId ==. key] [LimitTo 1]
+      case db of
+        [a] -> do
+          (id', path') <- liftIO $ Lambda.Trace.trace (entityVal a) (lasting body)
+          return $ toJSON $ StartTraceReply (show id') path' 
+        _ -> do
+          invalidArgs ["traceId"]
+    "STAP" -> do 
+      let key = STapKey (SqlBackendKey $ Handler.Lambda.traceId body)
+      db <- runDB $ selectList [STapId ==. key] [LimitTo 1]
+      case db of
+        [a] -> do
+          (id', path') <- liftIO $ Lambda.Trace.trace (entityVal a) (lasting body)
+          return $ toJSON $ StartTraceReply (show id') path' 
+        _ -> do
+          invalidArgs ["traceId"]
+    _ -> do 
+      invalidArgs ["traceType"]
 
 
 
